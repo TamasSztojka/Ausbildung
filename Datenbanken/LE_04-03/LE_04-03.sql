654b28609e23f3fbb8332a478c61209d5b4a5c11
TamasSztojka <tsztojka1@gmail.com>
2025-07-22 13:07:24 +0200

    Finished LE_04-03. Missed something earlier

--- Datenbanken/LE_04-03/LE_04-03.sql
+++ Datenbanken/LE_04-03/LE_04-03.sql
@@ -18,8 +18,25 @@ CREATE TABLE Productionsmaschinen(
     maschinenID INT,
     variante INT,
     bezeichnung VARCHAR(200),
-    UNIQUE(globalID, maschinenID, variante)
+    UNIQUE(maschinenID, variante)
 );
 
 INSERT INTO productionsmaschinen(maschinenID, variante, bezeichnung)
-VALUES (1, 1, 'Maschine Alpha'), (1, 2, 'Maschine Alpha Plus'), (2, 1, 'Maschine Beta'), (2, 2, 'Maschine Beta Plus')
+VALUES (1, 1, 'Maschine Alpha'), (1, 2, 'Maschine Alpha Plus'), (2, 1, 'Maschine Beta'), (2, 2, 'Maschine Beta Plus');
+
+DROP TABLE productionsmaschinen;
+
+CREATE TABLE Productionsmaschinen(
+	globalID INT AUTO_INCREMENT PRIMARY KEY,
+    laufzeit INT,
+    maxlaufzeit INT,
+    maschinenID INT,
+    variante INT,
+    bezeichnung VARCHAR(200),
+    UNIQUE(maschinenID, variante),
+    CONSTRAINT Productionsmaschinen CHECK(laufzeit < maxlaufzeit)
+);
+
+INSERT INTO productionsmaschinen(maschinenID, variante, bezeichnung)
+VALUES (1, 1, 'Maschine Alpha'), (1, 2, 'Maschine Alpha Plus')
+
