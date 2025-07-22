88642e94977d322ac485b8d44547658f7712cec8
TamasSztojka <tsztojka1@gmail.com>
2025-07-22 12:56:38 +0200

    Finished LE_04-03

---
+++ Datenbanken/LE_04-03/LE_04-03.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE constraints_datenbank;
+USE constraints_datenbank;
+
+CREATE TABLE productionsmaschinen(
+	maschinenID INT,
+    variante INT,
+    bezeichnung VARCHAR(200),
+    PRIMARY KEY(maschinenID, variante)
+);
+
+INSERT INTO productionsmaschinen(maschinenID, variante, bezeichnung)
+VALUES(1, 1, 'Maschine Alpha'), (1, 2, 'Maschine Alpha Plus'), (2, 1, 'Maschine Beta'),(2, 2, 'Maschine Beta Plus');
+
+DROP TABLE productionsmaschinen;
+
+CREATE TABLE Productionsmaschinen(
+	globalID INT AUTO_INCREMENT PRIMARY KEY,
+    maschinenID INT,
+    variante INT,
+    bezeichnung VARCHAR(200),
+    UNIQUE(globalID, maschinenID, variante)
+);
+
+INSERT INTO productionsmaschinen(maschinenID, variante, bezeichnung)
+VALUES (1, 1, 'Maschine Alpha'), (1, 2, 'Maschine Alpha Plus'), (2, 1, 'Maschine Beta'), (2, 2, 'Maschine Beta Plus')
