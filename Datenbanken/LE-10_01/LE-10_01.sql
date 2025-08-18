cf4204842d5313b8d59cbbf604c2b0781fd9a471
TamasSztojka <tsztojka1@gmail.com>
2025-08-18 15:54:16 +0200

    Finished LE_10_01

--- Datenbanken/LE-10_01/LE-10_01.sql
+++ Datenbanken/LE-10_01/LE-10_01.sql
@@ -11,4 +11,39 @@ JOIN mitarbeiter
     ON sachpraemie.mitarbeiterid = mitarbeiter.mitarbeiterid;
     
 -- Punkt 2
+SELECT 
+    m.name,
+    m.vorname,
+    sp.summepreis
+FROM mitarbeiter m
+JOIN (
+    SELECT 
+        mitarbeiterid,
+        SUM(preis) AS summepreis
+    FROM sachpraemie
+    GROUP BY mitarbeiterid
+) sp
+ON m.MitarbeiterID = sp.mitarbeiterid;
 
+-- Punkt 3
+SELECT 
+    m.vorname,
+    m.name,
+    m.steuerklasse
+FROM mitarbeiter m
+LEFT JOIN steuerklasse 
+       ON m.steuerklasse = s.steuerklassezahl
+WHERE s.steuerklassezahl IS NULL;
+
+-- Punk 4
+SELECT 
+    m.vorname,
+    m.name,
+    m.steuerklasse
+FROM mitarbeiter AS m
+WHERE m.steuerklasse IS NOT NULL
+  AND NOT EXISTS (
+    SELECT s.steuerklassezahl
+    FROM steuerklasse s
+    WHERE s.steuerklassezahl = m.steuerklasse
+);
