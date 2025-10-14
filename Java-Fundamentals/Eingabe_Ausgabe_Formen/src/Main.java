eb07f796384929f74aa6469d25d7f6220e0ddd76
TamasSztojka <tsztojka1@gmail.com>
2025-10-14 07:33:49 +0200

    finished LE_02_03

--- Java-Fundamentals/Eingabe_Ausgabe_Formen/src/Main.java
+++ Java-Fundamentals/Eingabe_Ausgabe_Formen/src/Main.java
@@ -1,9 +1,11 @@
 void main(){
+    String again;
+    boolean continue_while = true;
     int choice;
     double length, width, height, side, radius;
     double base = 0;
     double volume = 0;
-    while(true){
+    while(continue_while) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("=== Program to calculate base and volume of an object. ===\n");
@@ -14,7 +16,7 @@ void main(){
         System.out.println("Your Choice: ");
         choice = scanner.nextInt();
 
-        switch(choice){
+        switch (choice) {
             case 1:
                 System.out.println("Cuboid");
                 System.out.println("The base is a rectangle");
@@ -28,8 +30,8 @@ void main(){
                 base = length * width;
                 volume = base * height;
 
-                System.out.println("Base (rectangle): " + base );
-                System.out.println("Volume (Quadar): " + volume );
+                System.out.printf("Base (rectangle): %.2f%n", base);
+                System.out.printf("Volume (Quadar): %.2f%n", volume);
                 break;
 
             case 2:
@@ -43,8 +45,8 @@ void main(){
                 base = (Math.sqrt(3) / 4) * Math.pow(side, 2);
                 volume = base * height;
 
-                System.out.println("Base (triangle): " + base );
-                System.out.println("Volume (triangle): " + volume );
+                System.out.printf("Base (triangle): %.2f%n", base);
+                System.out.printf("Volume (triangle): %.2f%n", volume);
                 break;
 
             case 3:
@@ -58,13 +60,20 @@ void main(){
                 base = Math.PI * Math.pow(radius, 2);
                 volume = base * height;
 
-                System.out.println("base (Circle): " +  base);
-                System.out.println("Volume (Cylinder): " + volume);
+                System.out.printf("base (Circle): %.2f%n", base);
+                System.out.printf("Volume (Cylinder): %.2f%n", volume);
                 break;
 
             default:
                 System.out.println("Invalid choice. Please only enter the numbers 1-3.");
                 break;
         }
+        // === Ask if user wants to continue ===
+        System.out.print("\nWould you like to try again? (y/n): ");
+        again = scanner.next(); // read as word (not nextLine)
+        if (!again.trim().equalsIgnoreCase("y")) {
+            continue_while = false;
+        }
     }
+    System.out.print("Goodbye!");
 }
